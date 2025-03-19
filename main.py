44a63ba5fe1efbff9b659b1603ef94305241f45c
Kostas2104 <138314749+Kostas2104@users.noreply.github.com>
2025-03-19 09:18:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from datetime import datetime
 from get_gateio_caw_volume_in_usd import get_gateio_caw_volume_in_usd
 from get_prices import get_cmc_data
 from database import save_data
 
-
+# Get the current timestamp
+record_date = datetime.now()
 
 # Fetch data
 caw_volume_usd_gateio = get_gateio_caw_volume_in_usd()
 market_data = get_cmc_data()
 
-# Save to database
+# Save to database with timestamp
 if market_data:
-    save_data(market_data, caw_volume_usd_gateio)
-    print("Data saved to database successfully!")
+    save_data(record_date, market_data, caw_volume_usd_gateio)
+    print(f"Data saved to database successfully at {record_date}!")
