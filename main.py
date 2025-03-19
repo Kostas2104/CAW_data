e162ccaf9809c3d0ffed96a403f4fab643afc995
Kostas2104 <138314749+Kostas2104@users.noreply.github.com>
2025-03-19 09:01:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from get_gateio_caw_volume_in_usd import get_gateio_caw_volume_in_usd
 from get_prices import get_cmc_data
-from database import create_tables, save_data
+from database import save_data
 
-# Create database tables if they don't exist
-create_tables()
+
 
 # Fetch data
 caw_volume_usd_gateio = get_gateio_caw_volume_in_usd()
